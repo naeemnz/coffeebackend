37d32293ab8cce5fe25596508b6c10b98b7f6853
Nazifa <nazifanaimie@gmail.com>
2025-09-14 20:50:27 +0200

    github data added

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ const express = require('express')
 const app = express()
 const port = 4000 //jub env variables .env me define hojaye to sirf wohi port kam karenge locally, 4000 ki jagah .env me define port chalega, 4000 tab chalega jab .env me kuch define nahi hoga
 
+const githubData = {
+  "login": "kevinpowell",
+  "id": 915986,
+  "node_id": "MDQ6VXNlcjkxNTk4Ng==",
+  "avatar_url": "https://avatars.githubusercontent.com/u/915986?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/kevinpowell",
+  "html_url": "https://github.com/kevinpowell",
+  "followers_url": "https://api.github.com/users/kevinpowell/followers",
+  "following_url": "https://api.github.com/users/kevinpowell/following{/other_user}",
+  "gists_url": "https://api.github.com/users/kevinpowell/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/kevinpowell/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/kevinpowell/subscriptions",
+  "organizations_url": "https://api.github.com/users/kevinpowell/orgs",
+  "repos_url": "https://api.github.com/users/kevinpowell/repos",
+  "events_url": "https://api.github.com/users/kevinpowell/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/kevinpowell/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Kevin Powell",
+  "company": null,
+  "blog": "kevinpowell.guru",
+  "location": "Scappoose, OR",
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 15,
+  "public_gists": 4,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2011-07-14T20:09:14Z",
+  "updated_at": "2025-03-09T23:34:29Z"
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -20,6 +55,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
   res.send('<h4>Welcome to YouTube</h4>')
+})
+
+app.get('/github', (req, res) => {
+  res.json(githubData)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
